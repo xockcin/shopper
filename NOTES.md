784e368f00176aad2438b8aabee446a537fb3d80
Nick Cox <xoc.kcin@gmail.com>
2021-07-21 16:57:47 -0400

    completed very janky first draft which can add items but not remove them. a nice start.

---
+++ NOTES.md
@@ -0,0 +1,9 @@
+# 7/21/21
+
+I have been kicking this idea around in my head for a long time now. The time has come to sit down and bang it out. The plan is, I will build it so it works but is ugly - I will not waste any time at all getting hung up on how it should look. Then I will hand it off to my friend Megan, a newly-minted UX designer, who will figure out how it ought to look and feel, and then I will implement her changes, and then we will both use it as a portfolio project.
+
+So here's how it goes. You just got back from the grocery story with a big long paper receipt. You want to enter your groceries into some sort of spreadsheet, but typing them all in along with all the prices and everything is just a lot of work and not that fun. You probably won't do it.
+
+Enter Shopper (working title), an app that makes it easy and fun to itemize and keep track of your grocery shopping expenses. Choose which store you went to, then add each item with a single click of a button. You can also create new buttons if it's an item you've never bought before, adjust the price if the item was on sale etc., and so on. If it's a bulk item, you click the button and then enter the amount; if it's a nonbulk item, you just click it. Once you've entered all your items, you click a checkout button, and your receipt is recorded.
+
+There's a lot more that you could do with this basic setup. You could, for instance, keep track of what's in your pantry, track usage and waste, get notified when you are running low on something, etc. But I will save all that for later iterations.
--- src/App.js
+++ src/App.js
@@ -1,10 +1,84 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const stores = ["Pemberton", "Market Basket", "Trader Joes"]
+
+const mbItems = [
+  {
+    name: "Cabot Greek Yogurt",
+    price: 3.69
+  },
+  {
+    name: "Cabot Cheddar Cheese",
+    price: 8.99
+  },
+  {
+    name: "Nellie's Eggs",
+    price: 3.69
+  },
+  {
+    name: "Teddie Peanut Butter",
+    price: 2.99
+  }
+]
+
+const cartItems = []
+
+const storeList = stores.map(store => {
   return (
-    <div className="App">
-      
+    <div className="border">
+      <h1>{store}</h1>
+    </div>
+  )
+})
+
+const cartList = cartItems.map(item => {
+  return (
+    <div className="border">
+      <h1>
+        {item.name} - {item.price}
+      </h1>
+    </div>
+  );
+})
+
+function App() {
+  const [cartItems, setCartItems] = useState([])
+
+  const handleAddToCart = (item) => {
+    const currentItems = cartItems
+    setCartItems(cartItems.concat(item))
+  }
+
+  const cartList = cartItems.map((item) => {
+    return (
+      <div className="border">
+        <h1>
+          {item.name} - {item.price}
+        </h1>
+      </div>
+    );
+  });
+
+  const itemList = mbItems.map((item) => {
+    return (
+      <div className="border" onClick={() => handleAddToCart(item)}>
+        <h1>
+          {item.name} - {item.price}
+          <button>add</button>
+        </h1>
+      </div>
+    );
+  });
+
+  return (
+    <div className="row">
+      <div className="column">
+        {itemList}
+      </div>
+      <div className="column">
+        {cartList}
+      </div>
     </div>
   );
 }
